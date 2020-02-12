411217626846779d8a2ecb7755b26897d787aef1
zhanyha <1633086394@qq.com>
2020-02-13 00:13:03 +0800

    排序

--- lanqiao/src/com/zyh/lanqiao/dataStruct/tree/PTA/Q6_完全二叉搜索树.java
+++ lanqiao/src/com/zyh/lanqiao/dataStruct/tree/PTA/Q6_完全二叉搜索树.java
@@ -1,7 +1,13 @@
 package com.zyh.lanqiao.dataStruct.tree.PTA;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
+/**
+ * 先排序，然后中序遍历生成树。在对树层次遍历
+* @author zhanyuhao
+* @version 创建时间：2020年2月13日 上午12:10:04
+* 类说明
+ */
 public class Q6_完全二叉搜索树 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,7 +16,7 @@ public class Q6_完全二叉搜索树 {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        
+        Arrays.sort(arr);
         sc.close();
     }
 }
