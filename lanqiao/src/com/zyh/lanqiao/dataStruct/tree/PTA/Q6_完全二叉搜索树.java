a835ac7c36c7d64d6c34d0721e17b57ee05d93dc
zhanyha <1633086394@qq.com>
2020-02-12 16:40:47 +0800

    q6

--- lanqiao/src/com/zyh/lanqiao/dataStruct/tree/PTA/Q6_完全二叉搜索树.java
+++ lanqiao/src/com/zyh/lanqiao/dataStruct/tree/PTA/Q6_完全二叉搜索树.java
@@ -1,7 +1,16 @@
 package com.zyh.lanqiao.dataStruct.tree.PTA;
 
-public class Q5_完全二叉搜索树 {
+import java.util.Scanner;
+
+public class Q6_完全二叉搜索树 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
     }
 }
