7c407a6a0167b019403c858b3950e2fcba72c804
zhanyha <1633086394@qq.com>
2020-02-13 11:11:59 +0800

    Q7_笛卡尔树

---
+++ lanqiao/src/com/zyh/lanqiao/dataStruct/tree/PTA/Q7_笛卡尔树.java
@@ -0,0 +1,24 @@
+package com.zyh.lanqiao.dataStruct.tree.PTA;
+
+import java.util.Scanner;
+/**
+*https://pintia.cn/problem-sets/988034414048743424/problems/988041149056557056
+* @author zhanyuhao
+* @version 创建时间：2020年2月13日 上午1:02:30
+* 类说明
+ */
+//6
+//8 27 5 1
+//9 40 -1 -1
+//10 20 0 3
+//12 21 -1 4
+//15 22 -1 -1
+//5 35 -1 -1
+public class Q7_笛卡尔树 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        sc.close();
+    }
+}
